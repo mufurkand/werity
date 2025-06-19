463f0534318de370a9633bfb26d9126cd949e95e
mufurkand <mfurkand115@gmail.com>
2025-06-19 12:05:52 +0300

    Update project name

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Decentralized Social Media",
+  title: "Werity",
   description: "A decentralized social media platform",
 };
 
